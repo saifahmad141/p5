6f569b00c1604d053801620216101faceb290f90
saifahmadkhan786 <saif.ahmadkhan313@gmail.com>
2020-07-27 21:50:42 +0530

    first commit

---
+++ myapp1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from myapp1 import views
+app_name="myapp1"#is used to create a namespace\
+
+urlpatterns = [
+    #path('',views.index,name='index'),
+    #path('home',views.home,name="home"),
+    #path('fact/<n>',views.fact,name="fact"),
+    #path("secondarysuffix",address of function,name of mapping)
+    path('child/',views.child,name="child"),
+]
